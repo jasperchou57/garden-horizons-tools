98c8a00329ce9fda6078e65435149dc330495222
jasperchou57 <redmanhao@gmail.com>
2026-02-28 19:43:17 +0800

    添加Google Analytics跟踪代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7PEG24YXE0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7PEG24YXE0');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
